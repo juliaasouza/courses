65d6cbd9a1be5557ceadba3326affaf588649b53
julia <juliabbud@hotmail.com>
2022-05-04 14:32:32 -0300

    Feito o exercício 2 do curso "Entendendo Métodos Java"

---
+++ gft-start-woman-java/entendendo-metodos-java/src/dio/exerciciodois/Main.java
@@ -0,0 +1,12 @@
+package dio.exerciciodois;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Testes dos quadrilateros:");
+		Quadrilatero.calculaArea(4);
+		Quadrilatero.calculaArea(4, 3);
+		Quadrilatero.calculaArea(4, 3, 2);
+	}
+
+}
---
+++ gft-start-woman-java/entendendo-metodos-java/src/dio/exerciciodois/Quadrilatero.java
@@ -0,0 +1,16 @@
+package dio.exerciciodois;
+
+public class Quadrilatero {
+	
+	public static void calculaArea(double lado) {
+		System.out.println("A area do quadrado e " + lado * lado);
+	}
+	
+	public static void calculaArea(double lado1, double lado2) {
+		System.out.println("A area do retangulo e " + lado1 * lado2);
+	}
+	
+	public static void calculaArea(double baseMenor, double baseMaior, double altura) {
+		System.out.println("A area do trapezio e " + ((baseMenor + baseMaior) * altura) / 2);
+	}
+}
